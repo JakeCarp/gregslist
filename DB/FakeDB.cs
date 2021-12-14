64cbade06a1a4ea785a7b17fdbbf5b59e9e2841a
jacob carpenter <jakecarpenter407@gmail.com>
2021-12-14 14:07:58 -0700

    cars are done

--- DB/FakeDB.cs
+++ DB/FakeDB.cs
@@ -7,7 +7,7 @@ namespace gregslist.DB
         public static List<Car>? Cars { get; private set; } = new List<Car>()
         {
         {new Car("Toyota", "AE86", 1000, 10000, "It's a thing")},
-        {new Car('Honda', "Accord", 10, 20000, "New New")}
+        {new Car("Honda", "Accord", 10, 20000, "New New")}
         };
         public static List<House>? Houses { get; private set; } = new List<House>()
         {
--- Models/Car.cs
+++ Models/Car.cs
@@ -1,4 +1,4 @@
-using System.ComponentModel.DataAnnotations;
+
 
 namespace gregslist.Models
 {
@@ -14,15 +14,15 @@ namespace gregslist.Models
             //NOTE This line establishes a new globally unique ID
             Id = Guid.NewGuid().ToString(); ;
         }
-        [Required]
+
         public string Make { get; set; }
-        [Required]
+
         public string Description { get; set; }
-        [Required]
+
         public int Miles { get; set; }
-        [Required]
+
         public int Price { get; set; }
-        [Required]
+
         public string Model { get; set; }
         public string? Id { get; set; }
         // NOTE This is the method of requiring a property, only  applys to the one that immediatly follows
